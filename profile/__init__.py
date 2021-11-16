4a3f369d725ebb2958170fcbc6719ab95bac753a
ColinFChevalier <colinfchevalier@gmail.com>
2021-11-15 21:11:32 -0600

    added get all users

---
+++ profile/__init__.py
@@ -0,0 +1,1 @@
+from .requests import get_all_users
--- profile/requests.py
+++ profile/requests.py
@@ -3,6 +3,40 @@ import sqlite3
 
 PROFILES = []
 
+def get_profile_details():
+    # Open a connection to the database
+    with sqlite3.connect("./rare.db") as conn:
+
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            u.id,
+            u.first_name,
+            u.last_name,
+            u.email,
+            u.bio,
+            u.username,
+            u.password,
+            u.profile_img_url,
+            u.created_on,
+            u.active
+        FROM user u
+        """)
+
+        users = []
+
+        dataset = db_cursor.fetchall()
+
+    for row in dataset:
+
+        user = User(row['id'], row['first_name'], row['last_name'], row['email'],
+                    row['bio'], row['username'], row['password'],
+                    row['profile_image_url'], row['created_on'], row['active'])
+
+    return json.dumps(users)
+
 def upload_profile_image(new_profile_image):
     with sqlite3.connect("./rare.db") as conn:
         db_cursor = conn.cursor()
@@ -16,7 +50,7 @@ def upload_profile_image(new_profile_image):
 
         id = db_cursor.lastrowid
         new_profile_image['id'] = id  
-        # Are lines 17 and 18 neccessary for a profile image, or is this
+        # Are above two lines neccessary for a profile image, or is this
         # just for create of a whole new user?
 
 def deactivate_user_profile(deactivate_user):
@@ -32,3 +66,5 @@ def deactivate_user_profile(deactivate_user):
 
         id = db_cursor.lastrowid
         deactivate_user['id'] = id
+
+    
